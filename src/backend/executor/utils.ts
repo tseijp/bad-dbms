36d59a6a1aa3daf7abdd39fca1c2de3d28f6394c
tseijp <_@tsei.jp>
2026-05-24 05:32:19 +0000

    chore: format of code

--- src/backend/executor/utils.ts
+++ src/backend/executor/utils.ts
@@ -8,12 +8,6 @@ export const collectRids = (firstHeap: { scan(emit: (rid: Rid) => boolean | void
         firstHeap.scan((rid: Rid) => void rids.push(rid))
         return rids
 }
-export const EMPTY_ITER: RowIterator = {
-        next() {
-                return null
-        },
-        close() {},
-}
 export const fromRows = (rows: Row[]): RowIterator => {
         let _i = 0
         return {
